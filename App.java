c3566001f90dbfc131a20ba717514680427de056
riyaa-devops <riyaseetharam@gmail.com>
2026-07-17 00:21:16 +0530

    Update output message in App.java

--- App.java
+++ App.java
@@ -2,4 +2,4 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Maven CI/CD Pipeline");
     }
-}
+}*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*/
